5be54e3a97d1a702ddaeca5e29cb6124eceeeb15
Connor Fewin <fewincj@appstate.edu>
2023-06-08 16:05:15 -0400

    Rounded the current price

--- backend/StockData.py
+++ backend/StockData.py
@@ -3,6 +3,6 @@ import yfinance as yf
 def getCurrentPrice(symbol):
     ticker = yf.Ticker(symbol)
     current_price = ticker.history(period='1d')['Close'].iloc[-1]
-    return current_price
-
+    rounded_price = round(current_price, 2)
+    return rounded_price
 
