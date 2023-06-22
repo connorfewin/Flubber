7f5d8b648db2212c8aa4d5ba788423ec7943cac3
Connor Fewin <fewincj@appstate.edu>
2023-06-22 11:28:13 -0400

    Started Work on Order History

---
+++ frontend/src/Components/OrderHistory.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const OrderHistory = () => {
+
+    const [orders, setOrders] = useState([]);
+    return(
+        <h2>Order History</h2>
+    );
+};
+
+export default OrderHistory;
