62acf7d680d7e7ad3643fbd5e0896162d5aa13f9
Connor Fewin <fewincj@appstate.edu>
2023-07-27 15:10:09 -0400

    Portfolio is stlyed up with a custom tooltip, time for backend connection

--- frontend/src/Components/PortfolioValueChart.jsx
+++ frontend/src/Components/PortfolioValueChart.jsx
@@ -8,7 +8,7 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
-
+import '../Styles/PortfolioValueChart.css'
 const data = [
   { name: "Jan", intialValue: 9000, tradeProfit : 100, bankTransfer: 100 },
   { name: "Feb", intialValue: 9000, tradeProfit: -150, bankTransfer: 200 },
@@ -18,6 +18,21 @@ const data = [
   { name: "Jun", intialValue: 9000, tradeProfit: 980, bankTransfer: 600 },
   { name: "Jul", intialValue: 9000, tradeProfit: 1400, bankTransfer: 700 },
 ];
+
+function CustomTooltip({ payload, label, active }) {
+    if (active) {
+      return (
+        <div className="custom-tooltip">
+       <p className="bankTransferLabel">{`Bank Transfer Value: $${payload[2].value.toLocaleString("en-US", { minimumFractionDigits: 2 })}`}</p>
+        <p className="tradingProfitLabel">{`Trading Pofit: $${payload[1].value.toLocaleString("en-US", { minimumFractionDigits: 2 })}`}</p>
+          <p className="initialValueLabel">{`Initial Value: $${payload[0].value.toLocaleString("en-US", { minimumFractionDigits: 2 })}`}</p>
+          <p className="totalLabel">{`Total: $${(payload[0].value + payload[1].value + payload[2].value).toLocaleString("en-US", { minimumFractionDigits: 2 })}`}</p>
+        </div>
+      );
+    }
+  
+    return null;
+  }
 
 const PortfolioValueChart = ({ portfolio }) => {
   return (
@@ -37,27 +52,30 @@ const PortfolioValueChart = ({ portfolio }) => {
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
-          <YAxis type="number" allowDataOverflow={true} domain={[8000, 11500]}/>
-          <Tooltip />
+          <YAxis type="number" allowDataOverflow={true} domain={[8000, 12000]}/>
+          <Tooltip content={<CustomTooltip />}/>
           <Area
             type="monotone"
             dataKey="intialValue"
             stackId="1"
-            stroke="#8884d8"
+            stroke="#6D3CD8"
+            strokeWidth={2}
             fill="#8884d8"
           />
           <Area
             type="monotone"
             dataKey="tradeProfit"
             stackId="1"
-            stroke="#82ca9d"
+            stroke="#45C876"
+            strokeWidth={2}
             fill="#82ca9d"
           />
           <Area
             type="monotone"
             dataKey="bankTransfer"
             stackId="1"
-            stroke="#ffc658"
+            stroke="#FFAB39"
+            strokeWidth={2}
             fill="#ffc658"
           />
         </AreaChart>
