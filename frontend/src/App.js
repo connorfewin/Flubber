6634f1caa44d2cfa8aca4d4fc3490e42775228bc
Connor Fewin <fewincj@appstate.edu>
2023-07-28 10:02:00 -0400

    Made newWatchlist a modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/CreationZone" element={<NewWatchlist />} />
       </Routes>
     </Router>
     </div>
--- frontend/src/Containers/DisplayWatchlist.jsx
+++ frontend/src/Containers/DisplayWatchlist.jsx
@@ -1,19 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  fetchWatchlistsAPI,
-  fetchManySecuritiesAPI,
-} from "../api";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import { fetchWatchlistsAPI, fetchManySecuritiesAPI } from "../api";
 import { API, graphqlOperation } from "aws-amplify";
 import { onUpdateSecurity } from "../graphql/subscriptions";
 import "../Styles/Watchlist.css";
 import Security from "../Components/Security";
+import NewWatchlist from "../Components/NewWatchlist";
 
-const DisplayWatchlist = ({portfolio}) => {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "rgba(255, 255, 255, 0.9)",
+  border: "#DADADA",
+  borderRadius: 4,
+  boxShadow: 24,
+  p: 4,
+};
+
+const DisplayWatchlist = ({ portfolio }) => {
   const navigate = useNavigate();
   const [watchlist, setWatchlist] = useState([]);
   const [securities, setSecurities] = useState([]);
   const [selectedWatchlist, setSelectedWatchlist] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
 
   // fetchWatchlists
   useEffect(() => {
@@ -80,8 +99,9 @@ const DisplayWatchlist = ({portfolio}) => {
   };
 
   const handleNewWatchlist = () => {
-    navigate("/CreationZone");
+    setOpenModal(true);
   };
+  const handleClose = () => setOpenModal(false);
 
   return (
     <div className="watchlist-container">
@@ -100,15 +120,38 @@ const DisplayWatchlist = ({portfolio}) => {
             {watchlist.length === 1 && watchlist[0].name}
           </h2>
         )}
-        <button className="new-watchlist-button" onClick={handleNewWatchlist}>
-          New Watchlist
-        </button>
+        <div className="newWatchlist-modal">
+          <button className="new-watchlist-button" onClick={handleNewWatchlist}>
+            New Watchlist
+          </button>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={openModal}
+            onClose={handleClose}
+            closeAfterTransition
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+              backdrop: {
+                timeout: 500,
+              },
+            }}
+          >
+            <Fade in={openModal}>
+              <div className="modal-container">
+                <Box sx={style}>
+                  <NewWatchlist />
+                </Box>
+              </div>
+            </Fade>
+          </Modal>
+        </div>
       </div>
       {securities.length > 0 && (
         <div className="securities-list">
           <div className="securities-row">
             {securities.map((security) => (
-                <Security key={security.id} security={security} />
+              <Security key={security.id} security={security} />
             ))}
           </div>
         </div>
